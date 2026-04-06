83ac34b75fb45af62c8bdb1d97f8ca65efc00dac
LeeeeMinggg <amonguslevel2@gmail.com>
2026-04-06 23:38:45 +0700

    update home page

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
